74e499e7bcd28ec5bd523dce0899338e26afb8a1
WasathTheekshana <wasath.vt@gmail.com>
2024-06-02 21:43:51 +0530

    chore: ShadCN initialized into the project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "staticg.sportskeeda.com"
+      }
+    ]
+  }
+};
 
 export default nextConfig;
