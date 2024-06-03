f7fcdba7d9f0c31376fc5b31afb8042eef6aac6c
WasathTheekshana <wasath.vt@gmail.com>
2024-06-03 16:06:19 +0530

    feat: Azure AI API connect

--- src/actions/transcript.ts
+++ src/actions/transcript.ts
@@ -1,3 +1,84 @@
 'use server';
 
+import { AzureKeyCredential, ChatRequestMessage, ChatRequestSystemMessage, OpenAIClient } from "@azure/openai";
 
+const env = process.env;
+
+async function transcript(prevState: any, formData: FormData) {
+  console.log("PREVIOUS STATE: ", prevState);
+
+  const id = Math.random().toString(36);
+
+  if (
+    !env.AZURE_API_KEY ||
+    !env.AZURE_ENDPOINT ||
+    !env.AZURE_DEPLOYMENT_NAME ||
+    !env.AUZRE_DEPLOYMENT_COMPLETIONS_NAME
+  ) {
+    console.error("Azure credentials not found");
+    return {
+      sender: "",
+      response: "Azure credentials not found"
+    }
+  }
+
+  const file = formData.get("audio") as File;
+
+  if (file.size === 0) {
+    return {
+      sender: "",
+      response: "No audio file found",
+    }
+  }
+
+  console.log(">>", file);
+
+  const arrayBuffer = await file.arrayBuffer();
+  const audio = new Uint8Array(arrayBuffer);
+
+  console.log("== Transcribe Audio Sample ==")
+
+  const client = new OpenAIClient(
+    env.AZURE_ENDPOINT,
+    new AzureKeyCredential(env.AZURE_API_KEY)
+  );
+
+  const result = await client.getAudioTranscription(
+    env.AZURE_DEPLOYMENT_NAME,
+    audio
+  )
+  console.log(`Transcription: ${result.text}`)
+
+  const messages = [
+    {
+      role: "system",
+      content: "You are a helpful assistant. You will answer questions and reply I can't answer that if you don't know the answer",
+    },
+    {
+      role: "user",
+      content: result.text
+    }
+  ];
+
+  const completions = await client.getChatCompletions(
+    env.AUZRE_DEPLOYMENT_COMPLETIONS_NAME,
+    messages,
+    {
+      maxTokens: 128,
+    }
+  );
+
+  const response = completions.choices[0].message?.content;
+
+  console.log(prevState.sender, "+++", result.text);
+
+  return {
+    sender: result.text,
+    response: response,
+    id,
+  }
+
+
+}
+
+export default transcript;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,42 @@ import Image from "next/image";
 import { SettingsIcon } from "lucide-react";
 import Messages from "@/components/Messages";
 import Recorder, { mimeType } from "@/components/Recorder";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useFormState } from "react-dom";
+import transcript from "@/actions/transcript";
+
+const initialState = {
+  sender: "",
+  response: "",
+  id: "",
+}
+
+export type Message = {
+  sender: string;
+  response: string;
+  id: string;
+}
 
 export default function Home() {
   const fileRef = useRef<HTMLInputElement | null>(null);
   const submitButtonRef = useRef<HTMLButtonElement | null>(null);
+  const [state, formAction] = useFormState(transcript, initialState);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    if (state.response && state.sender) {
+      setMessages((messages) => [
+        {
+          sender: state.sender || "",
+          response: state.response || "",
+          id: state.id || "",
+        },
+        ...messages,
+      ])
+    }
+  }, [state])
 
   const uploadAudio = (blob: Blob) => {
-
     const file = new File([blob], "audio.webm", { type: mimeType });
 
     // Set the file as the value in the hidden input file field
@@ -25,6 +53,8 @@ export default function Home() {
       }
     }
   }
+
+  console.log(messages);
 
   return (
     <main className="bg-black h-screen overflow-hidden overflow-y-auto">
