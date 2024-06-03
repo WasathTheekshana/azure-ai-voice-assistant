6efa8d0672dc0511a8f83d642ef4db6ed3213621
WasathTheekshana <wasath.vt@gmail.com>
2024-06-03 18:26:55 +0530

    style: Message interface

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Message } from '@/app/page'
+import { ChevronDownCircle } from 'lucide-react'
 
-function Messages() {
+interface Props {
+  messages: Message[]
+}
+
+function Messages({ messages }: Props) {
   return (
-    <div className="flex flex-col min-h-screen pt-20">Messages</div>
+    <div className={`${messages?.length > 0 ? "pb-96" : "pb-52"} flex flex-col min-h-screen pt-20`}>
+      {!messages?.length && (
+        <div className='flex flex-col space-y-10 flex-1 items-center justify-end pl-6'>
+          <p className='text-gray-500 animate-pulse'>
+            Start a conversation with the voice assistant
+          </p>
+          <ChevronDownCircle size={64} className='animate-bounce text-gray-500' />
+        </div>
+      )}
+    </div>
   )
 }
 
